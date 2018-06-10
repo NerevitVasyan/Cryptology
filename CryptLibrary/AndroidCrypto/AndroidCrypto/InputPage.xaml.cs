903f2b4042e12cfbe87c637cd3254542fd713855
Vitaly <nerevit@mail.ru>
2018-06-10 20:19:27 +0300

    android app added

---
+++ CryptLibrary/AndroidCrypto/AndroidCrypto/InputPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CryptLibrary;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidCrypto
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InputPage : ContentPage
+	{
+        List<string> algorithms;
+          
+         public InputPage ()
+		{
+			InitializeComponent ();
+            algorithms = new List<string>
+            {
+                "Caesar",
+                "XOR"
+            };
+            Pick.ItemsSource = algorithms;
+            Pick.SelectedIndex = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            CryptLibrary.ICryptService ser = new Caesar();
+            OutputPage p = new OutputPage();
+            InputText.Text = ser.Crypt(InputText.Text, KeyInput.Text);
+        }
+        private void EncryptClick(object sender, EventArgs e)
+        {
+            CryptLibrary.ICryptService ser = new Caesar();
+            InputText.Text = ser.Decrypt(InputText.Text, KeyInput.Text);
+            
+        }
+    }
+}
---
+++ CryptLibrary/AndroidCrypto/AndroidCrypto/OutputPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndroidCrypto
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class OutputPage : ContentPage
+	{
+        public Label label2;
+		public OutputPage ()
+		{
+			InitializeComponent ();
+            label2 = Label1;
+        }
+	}
+}
